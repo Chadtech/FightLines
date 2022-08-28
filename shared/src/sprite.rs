b45f59abda926f15344bbacba8a510017c58fdfc
Chadtech <chadtech0@gmail.com>
2022-08-28 05:11:12 -0400

    Sprite module

---
+++ shared/src/sprite.rs
@@ -0,0 +1,29 @@
+#[derive(Clone)]
+pub enum Sprite {
+    GrassTile,
+}
+
+impl Sprite {
+    pub fn html_id(&self) -> String {
+        self.to_pieces().join("-")
+    }
+    pub fn to_pieces(&self) -> &[&str] {
+        match self {
+            Sprite::GrassTile => &["grass", "tile"],
+        }
+    }
+    pub fn to_file_name(&self) -> String {
+        let mut file = self
+            .to_pieces()
+            .into_iter()
+            .map(|p| p.to_string())
+            .collect::<Vec<_>>()
+            .join("_");
+
+        file.push_str(".png");
+
+        file
+    }
+}
+
+pub const ALL_SPRITES: &[Sprite] = &[Sprite::GrassTile];
