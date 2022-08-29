b067b6c9f23ee86b95c0e9e5a94d9e0cf1e08965
Chadtech <chadtech0@gmail.com>
2022-08-29 03:28:52 -0400

    Put tile into its own module

---
+++ shared/src/tile.rs
@@ -0,0 +1,21 @@
+use crate::sprite::Sprite;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
+pub enum Tile {
+    GrassPlain,
+}
+
+impl Tile {
+    pub fn to_sprite(&self) -> Sprite {
+        match self {
+            Tile::GrassPlain => Sprite::GrassTile,
+        }
+    }
+}
+
+pub const PIXEL_WIDTH: u16 = 32;
+pub const PIXEL_HEIGHT: u16 = 32;
+
+pub const PIXEL_WIDTH_FL: f64 = 32.0;
+pub const PIXEL_HEIGHT_FL: f64 = 32.0;
